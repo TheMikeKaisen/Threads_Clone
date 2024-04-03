ae06af5869c22524aa59567b15491534ab30e1e8
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-04-04 05:29:34 +0530

    add pre save function in user model

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -15,14 +15,11 @@ const signupUser = async(req, res) => {
             return res.status(400).json({message: "User already exists"})
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt)
-
         const newUser = new User({
             name, 
             email, 
             username, 
-            password: hashedPassword
+            password 
         })
 
         await newUser.save();
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcryptjs'
 
 const userSchema = Schema({
     name: {
@@ -35,6 +36,17 @@ const userSchema = Schema({
 
 
 }, {timestamps:true})
+
+// hashpassword before saving user
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
 
 const User = mongoose.model("User", userSchema)
 
