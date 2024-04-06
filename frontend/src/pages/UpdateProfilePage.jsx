bcc4fa07aabe54d742a3d0cb34c1d514af0e0abe
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-04-07 00:06:14 +0530

    connect update functionality with frontend and uploading profile picture using base 64 string

---
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -0,0 +1,112 @@
+'use client'
+
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+  Avatar,
+  AvatarBadge,
+  IconButton,
+  Center,
+} from '@chakra-ui/react'
+import { SmallCloseIcon } from '@chakra-ui/icons'
+
+export default function UserProfileEdit() {
+  return (
+    <Flex
+      align={'center'}
+      justify={'center'}
+      my={6}
+      >
+      <Stack
+        spacing={4}
+        w={'full'}
+        maxW={'md'}
+        bg={useColorModeValue('white', 'gray.dark')}
+        rounded={'xl'}
+        boxShadow={'lg'}
+        p={6}
+        my={12}>
+        <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+          User Profile Edit
+        </Heading>
+        <FormControl id="userName">
+          <Stack direction={['column', 'row']} spacing={6}>
+            <Center>
+              <Avatar size="xl" src="https://bit.ly/sage-adebayo" />
+                
+            </Center>
+            <Center w="full">
+              <Button w="full">Change Icon</Button>
+            </Center>
+          </Stack>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Full Name</FormLabel>
+          <Input
+            placeholder="John doe"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Username</FormLabel>
+          <Input
+            placeholder="johndoe"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            placeholder="your-email@example.com"
+            _placeholder={{ color: 'gray.500' }}
+            type="email"
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Bio</FormLabel>
+          <Input
+            placeholder="your bio..."
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+          />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input
+            placeholder="password"
+            _placeholder={{ color: 'gray.500' }}
+            type="password"
+          />
+        </FormControl>
+        <Stack spacing={6} direction={['column', 'row']}>
+          <Button
+            bg={'red.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'red.500',
+            }}>
+            Cancel
+          </Button>
+          <Button
+            bg={'blue.400'}
+            color={'white'}
+            w="full"
+            _hover={{
+              bg: 'blue.500',
+            }}>
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
+  )
+}
